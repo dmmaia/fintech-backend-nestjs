3ac6bd4151298cc99152138b1278b44e5f6a0b2e
dmmaia <david_mmaia@hotmail.com>
2026-03-27 06:26:06 -0300

    fix: password encryption added

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +11,9 @@ export class AuthService {
     ) {}
 
     async signIn(username: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOne(username);
-        if (user?.password !== pass) {
+        const user = await this.usersService.findOneByEmail(username);
+        const isMatch = await bcrypt.compare(pass, user?.password);
+        if (!isMatch) {
             throw new UnauthorizedException();
         }
         const payload = { sub: user.id, username: user.email };
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,6 +4,7 @@ import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './users.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -11,12 +12,19 @@ export class UsersService {
         @InjectRepository(User) private readonly accountRepository: Repository<User>
       ){}
 
-    create(createAccountDto: CreateUserDto) {
+    async create(createAccountDto: CreateUserDto) {
+        createAccountDto.password = await bcrypt.hash(createAccountDto.password , 10)
         return this.accountRepository.create(createAccountDto)
       }
 
     async findOne(id: string){
       const account = await this.accountRepository.findOneBy({id})
+      if(!account) throw new NotFoundException('User not found')
+      return account
+    }
+
+    async findOneByEmail(email: string){
+      const account = await this.accountRepository.findOneBy({email})
       if(!account) throw new NotFoundException('User not found')
       return account
     }
