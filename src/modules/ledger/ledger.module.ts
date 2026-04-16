2d7606165bb4b73057d2f9a94f58e1432b8d34ec
dmmaia <david_mmaia@hotmail.com>
2026-04-16 04:42:36 -0300

    fix(ledger): import fix

--- src/modules/ledger/ledger.module.ts
+++ src/modules/ledger/ledger.module.ts
@@ -3,9 +3,13 @@ import { LedgerService } from './ledger.service';
 import { LedgerController } from './ledger.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LedgerEntry } from './ledger.entity';
+import { AccountsModule } from '../accounts/accounts.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LedgerEntry])],
+  imports: [
+    TypeOrmModule.forFeature([LedgerEntry]),
+    AccountsModule
+  ],
   controllers: [LedgerController],
   providers: [LedgerService],
   exports: [LedgerService]
